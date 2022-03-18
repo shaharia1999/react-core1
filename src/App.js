8fe7a88e90decc1f1b6ec131c7a61b9af6c38530
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-03-18 10:28:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const nayeks=['shakib','alomgir','kober','omarSani'];
+  const naykas=['SISIR','Sbana','kapila','mowsomi'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayeks.map(nayek=><Person name={nayek}></Person>)
+      }{
+        naykas.map(nayka=><Person heart={nayka}></Person>)
+      }
+     {/* <Person name={nayeks[0]} naika='mowsomi'></Person>
+     <Person name ={nayeks[1]} naika='sabana'></Person>
+     <Person name={nayeks[2]}naika='kapila'></Person>
+     <Person name={nayeks[3]}naika='mousomi'></Person> */}
+   
+     <h1>noton component</h1>
+     <Joydev name='ojoy'phone='090808908'></Joydev>
+     <Joydev name='bejoy' phone='09908989'></Joydev>
+  
     </div>
   );
 }
-
+function Person(props){
+  return( <div className='person'>
+     <h1>{props.name}</h1>
+     <p> {props.heart}</p>
+     </div>
+  )
+}
+function Joydev(props){
+  return(
+    <div className='person'>
+      <h1>{props.name}</h1>
+      <p>Phone: {props.phone}</p>
+    </div>
+  )
+}
 export default App;
